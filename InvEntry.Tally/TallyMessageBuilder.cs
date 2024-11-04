1483a54008a4a5aff1fbbfbbcba94492299a4e9c
yogeshck <yogesh.jun5@gmail.com>
2024-11-04 20:21:16 +1100

    tally message

---
+++ InvEntry.Tally/TallyMessageBuilder.cs
@@ -0,0 +1,15 @@
+﻿namespace InvEntry.Tally;
+
+public class TallyMessageBuilder
+{
+    ENVELOPE _env;
+
+    public TallyMessageBuilder() 
+    {
+        _env = new ENVELOPE();
+        _env.HEADER = new HEADER();
+        _env.HEADER.TALLYREQUEST = "Import Data";
+
+        _env.BODY = new BODY();
+    }
+}
