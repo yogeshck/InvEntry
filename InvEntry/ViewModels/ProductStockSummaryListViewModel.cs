5e4ed3c5be64572d1083d556246d1b59d39ea8a9
misrc <kvelgen2010@gmail.com>
2024-12-27 18:12:42 +0530

    Product stock summary list view added

---
+++ InvEntry/ViewModels/ProductStockSummaryListViewModel.cs
@@ -0,0 +1,50 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using DevExpress.Mvvm;
+using DevExpress.Xpf.Grid;
+using InvEntry.Extension;
+using InvEntry.Models;
+using InvEntry.Services;
+using InvEntry.Utils.Options;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+
+namespace InvEntry.ViewModels;
+
+public partial class ProductStockSummaryListViewModel : ObservableObject
+{
+    private readonly IProductStockSummaryService _pStockSummaryService;
+    private readonly IDialogService _reportDialogService;
+
+    [ObservableProperty]
+    private ObservableCollection<ProductStockSummary> _pStockSummary;
+
+    /*    [ObservableProperty]
+        private DateSearchOption _searchOption;
+
+        [ObservableProperty]
+        private InvoiceHeader _SelectedInvoice;
+
+        [ObservableProperty]
+        private DateTime _Today = DateTime.Today;*/
+
+    public ProductStockSummaryListViewModel(IProductStockSummaryService pStockSummaryService,
+            [FromKeyedServices("ReportDialogService")] IDialogService reportDialogService)
+    {
+        _pStockSummaryService = pStockSummaryService;
+        _reportDialogService = reportDialogService;
+
+        Task.Run(RefreshStockSummaryAsync).Wait();
+    }
+
+    [RelayCommand]
+    private async Task RefreshStockSummaryAsync()
+    {
+        var pStockSummaryResult = await _pStockSummaryService.GetAll();
+        if (pStockSummaryResult is not null)
+            PStockSummary = new(pStockSummaryResult);
+    }
+
+}
---
+++ InvEntry/Views/ProductStockSummaryListView.xaml.cs
@@ -0,0 +1,31 @@
+﻿using DevExpress.Xpf.Grid;
+using DevExpress.Xpf.WindowsUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace InvEntry.Views
+{
+    /// <summary>
+    /// Interaction logic for StockSummaryView.xaml
+    /// </summary>
+    public partial class ProductStockSummaryListView : NavigationPage
+    {
+        public ProductStockSummaryListView()
+        {
+            InitializeComponent();
+        }
+    }
+}
