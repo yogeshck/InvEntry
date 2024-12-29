3aa033c8069f634ab54accf2e107d825ae446a2b
misrc <kvelgen2010@gmail.com>
2024-12-29 13:12:15 +0530

    New Invoice Dynamic Format

---
+++ InvEntry/Reports/XrNewInvoice24.cs
@@ -0,0 +1,16 @@
+﻿using DevExpress.XtraReports.UI;
+using System;
+using System.Collections;
+using System.ComponentModel;
+using System.Drawing;
+
+namespace InvEntry.Reports
+{
+    public partial class XrNewInvoice24 : DevExpress.XtraReports.UI.XtraReport
+    {
+        public XrNewInvoice24()
+        {
+            InitializeComponent();
+        }
+    }
+}
