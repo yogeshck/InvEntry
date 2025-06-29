8689b18a2cd8d83ff1f8171367b3f477f64825f2
misrc <kvelgen2010@gmail.com>
2025-06-29 10:51:05 +0530

    Old metal transfer ui

---
+++ InvEntry/ViewModels/OldMetalTransferEntryViewModel.cs
@@ -0,0 +1,26 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using DevExpress.CodeParser;
+using DevExpress.Mvvm;
+using DevExpress.Xpf.WindowsUI.Navigation;
+using InvEntry.Extension;
+using InvEntry.Models;
+using InvEntry.Reports;
+using InvEntry.Services;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvEntry.ViewModels;
+
+
+public partial class OldMetalTransferEntryViewModel: ObservableObject
+{
+
+
+}
+
+
