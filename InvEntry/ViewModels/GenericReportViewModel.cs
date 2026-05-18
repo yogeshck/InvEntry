db63141a428d1c01e4b0f8a48da27be1b08b2809
misrc <kvelgen2010@gmail.com>
2026-05-18 22:01:53 +1000

    removed unnecessary code

--- InvEntry/ViewModels/GenericReportViewModel.cs
+++ InvEntry/ViewModels/GenericReportViewModel.cs
@@ -141,21 +141,3 @@ public partial class GenericReportViewModel : ObservableObject
     }
 }
 
-public class GenericRow
-{
-    public string Col1 { get; set; }
-    public string Col2 { get; set; }
-    public string Col3 { get; set; }
-    public string Col4 { get; set; }
-    public string Col5 { get; set; }
-    public string Col6 { get; set; }
-    public string Col7 { get; set; }
-    public string Col8 { get; set; }
-    public string Col9 { get; set; }
-    public string Col10 { get; set; }
-    public string Col11 { get; set; }
-    public string Col12 { get; set; }
-    public string Col13 { get; set; }
-    public string Col14 { get; set; }
-    public string Col15 { get; set; }
-}
